e8b3f9366ad2392b6ca6e8e9a827fba19ffb8d15
Mukherjeedip <74142654+Mukherjeedip@users.noreply.github.com>
2023-10-05 14:45:51 +0530

    Add files via upload

---
+++ y.py
@@ -0,0 +1,13 @@
+p=int(input())
+while (p >0):
+	for i in range ( 2 ) :
+		p=p+1
+         print("loc=",p)
+         i f ( p ==0):
+				break
+	for i in range ( 4 ) :
+		p=p−1
+		 p r i n t ( " Loc = " , p )
+		 i f ( p ==0):
+			break
+print(" Fell in p i t at location " , p )
